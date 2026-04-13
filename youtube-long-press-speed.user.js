d02fbcc5261cc6630ab60411ee83bb0d96be6009
kqint <2792661625@qq.com>
2026-04-13 17:33:56 +0800

    添加了 @namespace

--- youtube-long-press-speed.user.js
+++ youtube-long-press-speed.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YouTube 右方向键长按倍速播放
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/kqint
 // @version      1.0
 // @description  接管右方向键：短按快进，长按倍速播放，暂停时操作自动播放
 // @author       kqint
